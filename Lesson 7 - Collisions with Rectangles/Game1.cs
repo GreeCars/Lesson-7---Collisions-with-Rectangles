6074956836fe82acdd84aa1d43c592ced193b712
Carsen Greenwood <362570749@GoTVDSB.ca>
2024-12-12 09:38:21 -0500

    Working

--- Lesson 7 - Collisions with Rectangles/Game1.cs
+++ Lesson 7 - Collisions with Rectangles/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
 
 namespace Lesson_7___Collisions_with_Rectangles
 {
@@ -27,7 +28,7 @@ namespace Lesson_7___Collisions_with_Rectangles
         Rectangle barrierRect1, barrierRect2;
 
         Texture2D coinTexture;
-        Rectangle coinRect;
+        List<Rectangle> coins;
 
         Rectangle window;
 
@@ -44,20 +45,81 @@ namespace Lesson_7___Collisions_with_Rectangles
             // TODO: Add your initialization logic here
 
             base.Initialize();
+
+            pacSpeed = Vector2.Zero;
+            pacRect = new Rectangle(10, 10, 60, 60);
+
+            barrierRect1 = new Rectangle(0, 250, 350, 75);
+            barrierRect2 = new Rectangle(450, 250, 350, 75);
+
+            coins = new List<Rectangle>();
+            coins.Add(new Rectangle(400, 50, coinTexture.Width, coinTexture.Height));
+            coins.Add(new Rectangle(475, 50, coinTexture.Width, coinTexture.Height));
+            coins.Add(new Rectangle(200, 300, coinTexture.Width, coinTexture.Height));
+            coins.Add(new Rectangle(400, 300, coinTexture.Width, coinTexture.Height));
+
+            exitRect = new Rectangle(700, 380, 100, 100);
+
+            window = new Rectangle(0, 0, 800, 480);
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
+            pacDownTexture = Content.Load<Texture2D>("PacDown");
+            pacUpTexture = Content.Load<Texture2D>("PacUp");
+            pacRightTexture = Content.Load<Texture2D>("PacRight");
+            pacLeftTexture = Content.Load<Texture2D>("PacLeft");
+            currentPacTexture = pacRightTexture;
+
+            barrierTexture = Content.Load<Texture2D>("rock_barrier");
+
+            exitTexture = Content.Load<Texture2D>("hobbit_door");
+
+            coinTexture = Content.Load<Texture2D>("coin");
+
             // TODO: use this.Content to load your game content here
         }
 
         protected override void Update(GameTime gameTime)
         {
+            keyboardState = Keyboard.GetState();
+
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
+            pacSpeed = Vector2.Zero;
+            if (keyboardState.IsKeyDown(Keys.Left))
+            {
+                pacSpeed.X -= 2;
+                currentPacTexture = pacLeftTexture;
+            }
+            if (keyboardState.IsKeyDown(Keys.Right))
+            {
+                pacSpeed.X += 2;
+                currentPacTexture = pacRightTexture;
+            }
+            if (keyboardState.IsKeyDown(Keys.Up))
+            {
+                pacSpeed.Y -= 2;
+                currentPacTexture = pacUpTexture;
+            }
+            if (keyboardState.IsKeyDown(Keys.Down))
+            {
+                pacSpeed.Y += 2;
+                currentPacTexture = pacDownTexture;
+            }
+            pacRect.Offset(pacSpeed);
+
+            for (int i = 0; i < coins.Count; i++)
+            {
+                if (pacRect.Intersects(coins[i]))
+                {
+                    coins.RemoveAt(i);
+                    i--;
+                }
+            }
             // TODO: Add your update logic here
 
             base.Update(gameTime);
@@ -68,7 +130,16 @@ namespace Lesson_7___Collisions_with_Rectangles
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            _spriteBatch.Begin();
 
+            _spriteBatch.Draw(barrierTexture, barrierRect1, Color.White);
+            _spriteBatch.Draw(barrierTexture, barrierRect2, Color.White);
+            _spriteBatch.Draw(exitTexture, exitRect, Color.White);
+            _spriteBatch.Draw(currentPacTexture, pacRect, Color.White);
+            foreach (Rectangle coin in coins)
+                _spriteBatch.Draw(coinTexture, coin, Color.White);
+
+            _spriteBatch.End();
             base.Draw(gameTime);
         }
     }
